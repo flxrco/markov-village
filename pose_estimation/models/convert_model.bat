573c032dea7dc479635528642b750b86a4badbd9
Jon Felix Rico <jonfelixrico@gmail.com>
2018-11-30 14:14:23 +0800

    Add pose estimation API

---
+++ pose_estimation/models/convert_model.bat
@@ -0,0 +1,3 @@
+python convert_model.py posenet pose_iter_440000.caffemodel coco_posenet.npz
+python convert_model.py facenet pose_iter_116000.caffemodel facenet.npz
+python convert_model.py handnet pose_iter_102000.caffemodel handnet.npz
